ee0e988ba774b698ab1ca01a92c632e3e919f500
GrntKim <grantjun0224@gmail.com>
2026-05-11 16:17:05 +0900

    Add react router for SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import Footer from "./components/Layouts/Footer/Footer"
-import Header from "./components/Layouts/Header/Header"
-import Timer from "./components/Timer/Timer"
-import "./App.css"
+import { Routes, Route } from 'react-router-dom';
+import Footer from "./components/Layouts/Footer/Footer";
+import Header from "./components/Layouts/Header/Header";
+import Timer from "./components/Timer/Timer";
+import "./App.css";
+
+function Menu1() {
+  return <main>Menu1</main>
+}
+
+function Menu2() {
+  return <main>Menu2</main>
+}
+
+function Menu3() {
+  return <main>Menu3</main>
+}
 
 function App() {
 
   return (
       <div className="app-container">
         <Header></Header>
-        <Timer></Timer>
+
+        <main className="page">
+          <Routes>
+            <Route path='/' element={<Timer />}></Route>
+            <Route path='/menu1' element={<Menu1 />}></Route>
+            <Route path='/menu2' element={<Menu2 />}></Route>
+            <Route path='/menu3' element={<Menu3 />}></Route>
+          </Routes>
+        </main>
+
         <Footer></Footer>
       </div>
   );
--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,16 +1,17 @@
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
     return (
         <header>
             <div className="logo">
-                <a href="/">GK-TIMER <em>BETA</em></a>
+                <Link to="/">GK-TIMER <em>BETA</em></Link>
             </div>
             <div className="menus">
                 <ul className="list-menus">
-                    <li><a href="#">Menu1</a></li>
-                    <li><a href="#">Menu2</a></li>
-                    <li><a href="#">Menu3</a></li>
+                    <li><NavLink to="/menu1">Menu1</NavLink></li>
+                    <li><NavLink to="/menu2">Menu2</NavLink></li>
+                    <li><NavLink to="/menu3">Menu3</NavLink></li>
                 </ul>
             </div>
         </header>
