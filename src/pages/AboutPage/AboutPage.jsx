d689349387ffccbe86d70184d2c5e7febf1e8a2e
GrntKim <grantjun0224@gmail.com>
2026-05-12 08:55:47 +0900

    Add some contents to about page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,18 @@
+import { SiReact, SiVite } from 'react-icons/si';
 import './AboutPage.css';
 
 export default function AboutPage() {
     return (
         <div className="about-page">
-            TODO: Implement about page
+            <h1 className='title'>ABOUT</h1>
+            <div className="contents">
+                <p>GK-Timer: Yet another simple Twisty puzzle timer</p>
+                <p>Developed by: Junmyeong Kim(Grant Kim)</p>
+                <span>Powered by:
+                    <SiReact size={24} color='#61dafb'/>
+                    <SiVite size={24} color='#646cff'/>
+                </span>
+            </div>
         </div>
     );
 }
